59bc93a524c833e51b47cdbbc47d0da870712247
1114588423@qq.com <1114588423@qq.com>
2019-05-03 21:28:36 +0800

    C创建了项目

---
+++ mygit/src/main/java/com/chan/User.java
@@ -0,0 +1,13 @@
+package com.chan;
+
+/**
+ * @ClassName User
+ * @Description TODO
+ * @Author Chan
+ * @Date 2019/5/3 21:26
+ * @Version 1.0
+ **/
+public class User {
+    private Integer id;
+
+}
